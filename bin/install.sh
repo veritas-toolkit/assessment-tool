92a5dd3bc51f40f29d355f9e7cfaf01046fc788c
Si Xu <xusi1122@gmail.com>
2021-12-14 01:10:14 +0800

    [Mod] java opt args

--- bin/install.sh
+++ bin/install.sh
@@ -5,8 +5,12 @@ cd py || exit
 
 pip3 install virtualenv
 
+if [[ -d venv ]]; then
+  rm -rf venv
+fi
+# create virtual environment
 virtualenv venv -p python3
-
+# using virtual environment
 source venv/bin/activate
-
+# install python module
 pip3 install --no-cache-dir -r requirements.txt
