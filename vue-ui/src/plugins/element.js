7fc03f05d056ffd32e7fa9bc05304b0f0170db44
Si Xu <xusi1122@gmail.com>
2022-07-21 16:22:47 +0800

    [Fix] submodule vue-ui

--- vue-ui/src/plugins/element.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import Element from 'element-ui'
-import locale from 'element-ui/lib/locale/lang/en'
-import 'element-ui/lib/theme-chalk/index.css'
-
-Vue.use(Element,{locale})
