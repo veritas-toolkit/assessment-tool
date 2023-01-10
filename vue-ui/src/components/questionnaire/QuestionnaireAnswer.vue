5d3f818a8f45c1760d6cd757cfcf46212f0a9088
cp <462433864@qq.com>
2023-01-10 14:53:41 +0800

    [add]questionnaire answer structure

--- vue-ui/src/components/questionnaire/QuestionnaireAnswer.vue
+++ vue-ui/src/components/questionnaire/QuestionnaireAnswer.vue
@@ -1,36 +1,89 @@
 <template>
-  <div>
-    <div style="font-size: 40px">111</div>
-    <div style="font-size: 40px">111</div>
-    <div style="font-size: 40px">111</div>
-    <div style="font-size: 40px">111</div>
-    <div style="font-size: 40px">111</div>
-    <div style="font-size: 40px">111</div>
-    <div style="font-size: 40px">111</div>
-    <div style="font-size: 40px">111</div>
-    <div style="font-size: 40px">111</div>
-    <div style="font-size: 40px">111</div>
-    <div style="font-size: 40px">111</div>
-    <div style="font-size: 40px">111</div>
-    <div style="font-size: 40px">111</div>
-    <div style="font-size: 40px">111</div>
-    <div style="font-size: 40px">111</div>
-    <div style="font-size: 40px">111</div>
-    <div style="font-size: 40px">111</div>
-    <div style="font-size: 40px">111</div>
-    <div style="font-size: 40px">111</div>
-    <div style="font-size: 40px">111</div>
-    <div style="font-size: 40px">111</div>
-
+  <div style="padding: 16px 24px">
+    <div class="main-ques BarlowBold">F5. Have you documented how are these impacts being mitigated?</div>
+    <div class="sub-ques">
+      <div class="sub-ques-title">
+        <div style="padding: 8px 16px;font-size: 16px" class="BarlowMedium">
+          What are the measures of harms and benefits in the marketing system?
+        </div>
+        <div style="display: flex">
+          <img class="subQues-edit" src="../../assets/questionnairePic/edit.svg" alt="">
+          <img class="subQues-com" src="../../assets/questionnairePic/comment.svg" alt="">
+        </div>
+      </div>
+      <div class="sub-ques-ans BarlowMedium">
+        Benefit to customers from acquiring the loan due to marketing system: proportional to profit
+        - Harm to customers from a failed application, Z(Acq=0, App=1, A=a), estimated as described in Response B3 on each group, a.
+        - Benefit of receiving the intervention (demographic parity), P(S=1|A=a), measured directly from the marketing intervention selection system.
+        - Harm from a default: not addressed in this example for the |
+      </div>
+    </div>
+    <div class="sub-ques">
+      <div class="sub-ques-title">
+        <div style="padding: 8px 16px;font-size: 16px" class="BarlowMedium">
+          What are the measures of harms and benefits in the marketing system?
+        </div>
+        <div style="display: flex">
+          <img class="subQues-edit" src="../../assets/questionnairePic/edit.svg" alt="">
+          <img class="subQues-com" src="../../assets/questionnairePic/comment.svg" alt="">
+        </div>
+      </div>
+      <div class="sub-ques-ans BarlowMedium">
+        Benefit to customers from acquiring the loan due to marketing system: proportional to profit
+        - Harm to customers from a failed application, Z(Acq=0, App=1, A=a), estimated as described in Response B3 on each group, a.
+        - Benefit of receiving the intervention (demographic parity), P(S=1|A=a), measured directly from the marketing intervention selection system.
+        - Harm from a default: not addressed in this example for the |
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "QuestionnaireAnswer"
+  name: "QuestionnaireAnswer",
+  created() {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+.main-ques {
+  font-size: 16px;
+}
+.sub-ques-title {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 16px;
+  background: #F5F7F9;
+  border-radius: 4px 4px 0px 0px;
+  border: 1px solid #D5D8DD;
+}
+.subQues-edit {
+  width: 24px;
+  height: 24px;
+  margin-right: 8px;
+}
+.subQues-com {
+  width: 24px;
+  height: 24px;
+  margin-right: 16px;
+}
+.sub-ques-ans {
+  font-size: 16px;
+  padding: 8px 16px;
+  border-radius: 0px 0px 4px 4px;
+  border-right: 1px solid #D5D8DD;
+  border-left: 1px solid #D5D8DD;
+  border-bottom: 1px solid #D5D8DD;
+}
 </style>
