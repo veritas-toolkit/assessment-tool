ccb8f3a3e3c7652f16761910c1bc85bfcdc9bd42
cp <462433864@qq.com>
2023-01-16 11:21:48 +0800

    [mod]package vue/cli version 4.5.19 & target

--- vue-ui/vue.config.js
+++ vue-ui/vue.config.js
@@ -2,8 +2,8 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        // target: 'http://127.0.0.1:8001',
-        target: 'http://47.93.16.216:8001/',
+        target: 'http://127.0.0.1:8001',
+        // target: 'http://47.93.16.216:8001/',
         changeOrigin: true,
         pathRewrite: {
           '^/api': '/api'
