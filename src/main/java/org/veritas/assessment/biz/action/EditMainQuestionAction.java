cfcb6750747df3571e2a96d91f0173619dd8894a
Si Xu <xusi1122@gmail.com>
2023-02-15 16:36:43 +0800

    [Add] edit main question

---
+++ src/main/java/org/veritas/assessment/biz/action/EditMainQuestionAction.java
@@ -0,0 +1,27 @@
+package org.veritas.assessment.biz.action;
+
+import lombok.Data;
+import org.veritas.assessment.system.entity.User;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+public class EditMainQuestionAction {
+    @NotNull
+    private Integer projectId;
+
+    @NotNull
+    private Long basedQuestionnaireVid;
+
+    @NotNull
+    private Long questionId;
+
+    @NotNull
+    private Long basedQuestionVid;
+
+    @NotNull
+    private String question;
+
+    @NotNull
+    private User operator;
+}
---
+++ src/main/java/org/veritas/assessment/biz/dto/v2/questionnaire/QuestionEditDto.java
@@ -0,0 +1,25 @@
+package org.veritas.assessment.biz.dto.v2.questionnaire;
+
+import lombok.Data;
+import org.veritas.assessment.biz.action.AddMainQuestionAction;
+import org.veritas.assessment.biz.constant.AssessmentStep;
+import org.veritas.assessment.biz.constant.Principle;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Collections;
+import java.util.List;
+
+@Data
+public class QuestionEditDto {
+
+    @NotNull
+    private Long questionId;
+    @NotNull
+    private Long basedQuestionnaireVid;
+    @NotNull
+    private Long basedQuestionVid;
+    @NotNull
+    private String question;
+
+}
