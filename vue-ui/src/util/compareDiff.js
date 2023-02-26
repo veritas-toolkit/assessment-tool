8bf2cc93e9d5d82fbe16e155ecc3307f6b7e08de
Si Xu <xusi1122@gmail.com>
2023-02-26 21:03:02 +0800

    [Fix] diff words mode

--- vue-ui/src/util/compareDiff.js
+++ vue-ui/src/util/compareDiff.js
@@ -1,5 +1,12 @@
 import {diff_match_patch} from "diff-match-patch";
+import 'diff-match-patch-line-and-word';
 
+function compareDiff(string1,string2) {
+    var dmp = new diff_match_patch();
+    const diffs = dmp.diff_wordMode(string1, string2);
+    return diffs;
+}
+/*
 function compareDiff(string1,string2) {
     let dmp = new diff_match_patch();
     let diff = dmp.diff_main(string1,string2)
@@ -7,4 +14,5 @@ function compareDiff(string1,string2) {
     // let diffHtml = dmp.diff_prettyHtml(diff);// trans to html
     return diff
 }
+*/
 export {compareDiff}
