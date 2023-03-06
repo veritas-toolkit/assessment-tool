78ea5b587fa68e67d710e77a274f3d105630574d
cp <462433864@qq.com>
2023-03-06 17:18:28 +0800

    [add] admin template menu add delete edit recorder

--- vue-ui/src/components/adminTemplate/AdminTemplate.vue
+++ vue-ui/src/components/adminTemplate/AdminTemplate.vue
@@ -1,20 +1,197 @@
 <template>
-  <div>
-    {{templateId}}
+  <div style="height: 100%">
+    <el-container style="height: calc(100% - 1px);display: flex;flex-direction: column">
+      <el-header height="64px">
+        <div class="title BarlowBold">
+          <img class="backPic" src="../../assets/groupPic/back.png" @click="$router.back()" alt="" style="cursor: pointer">
+          <!--<router-link :to="{path:'/projectPage',query: {id:projectId}}"><img class="backPic" src="../../assets/groupPic/back.png" alt=""></router-link>-->
+          <span>Template</span>
+        </div>
+        <div class="BarlowMedium">
+          <el-radio-group v-model="principle" class="principle-group">
+            <el-radio-button v-show="this.principleList.indexOf('G') != -1" label="Generic"></el-radio-button>
+            <el-radio-button v-show="this.principleList.indexOf('F') != -1" label="Fairness"></el-radio-button>
+            <el-radio-button v-show="this.principleList.indexOf('EA') != -1" label="Ethics & Accountability"></el-radio-button>
+            <el-radio-button v-show="this.principleList.indexOf('T') != -1" label="Transparency"></el-radio-button>
+          </el-radio-group>
+        </div>
+        <div class="BarlowMedium">
+          <div id="save">Save</div>
+        </div>
+      </el-header>
+      <!--flex-direction: column; overflow-y: auto-->
+      <el-container style="flex: 1;overflow-y: auto">
+        <el-aside width="400px">
+          <AdminTemplateMenu @getId="getQuestionId" @getEditFlag="getEditFlag" :principle="principle" :defaultId="defaultId" :templateId="templateId" :menuData="menuData"></AdminTemplateMenu>
+        </el-aside>
+        <el-main>
+          <AdminTemplateSubQuestion @updateFlag="updateValue" :editFlag="editFlag" :addSubQuesFlag="addSubQuesFlag" :templateId="templateId" :questionId="questionId"></AdminTemplateSubQuestion>
+        </el-main>
+      </el-container>
+      <el-footer style="height: 64px;">
+        <div style="display: flex;width: 100%;justify-content:space-between;align-items: center">
+          <div class="notification">
+            <el-popover
+                placement="top-start"
+                width="400"
+                trigger="click">
+              <Notifications></Notifications>
+              <div class="not-box" slot="reference">
+                <img src="../../assets/projectPic/notification.png" alt="">
+                <span class="BarlowBold">Notifications</span>
+                <div>6</div>
+              </div>
+            </el-popover>
+          </div>
+          <div class="footer-right BarlowMedium draft-popover"  style="width: calc(100% - 400px)">
+            <div style="display: flex;justify-content: right">
+              <div class="add-subques" @click="addSubQuesFlag = true">
+                <img class="watch" src="../../assets/questionnairePic/watch.svg" alt="">
+                Add subquestion
+              </div>
+            </div>
+          </div>
+        </div>
+      </el-footer>
+    </el-container>
   </div>
 </template>
 
 <script>
+import Notifications from "@/components/comment/Notifications";
+import AdminTemplateMenu from "@/components/adminTemplate/AdminTemplateMenu";
+import AdminTemplateSubQuestion from "@/components/adminTemplate/AdminTemplateSubQuestion";
+
 export default {
   name: "AdminTemplate",
+  components: {
+    Notifications,
+    AdminTemplateMenu,
+    AdminTemplateSubQuestion,
+  },
   data() {
     return {
       templateId: this.$route.query.templateId,
+      principle: 'Generic',
+      questionId: '',
+      defaultId: '',
+      principleMap: {
+        "Generic" : "G",
+        "Fairness" : "F",
+        "Ethics & Accountability" : "EA",
+        "Transparency" : "T"
+      },
+      principleList: [],
+      menuData: [],
+      addSubQuesFlag: false,
+      editFlag: false,
     }
+  },
+  created() {
+    this.getQuestionnaireMenu()
+  },
+  watch: {
+    'principle': function () {
+      this.getQuestionnaireMenu()
+    }
+  },
+  methods: {
+    updateValue(data) {
+      this.addSubQuesFlag = data
+    },
+    getEditFlag(data) {
+      this.editFlag = data
+    },
+    getQuestionId(data) {
+      this.questionId = data
+    },
+    getQuestionnaireMenu() {
+      this.$http.get(`/api/admin/questionnaire/${this.templateId}/toc`).then(res => {
+        if (res.status == 200) {
+          this.principleList = Object.keys(res.data.principles)
+          this.menuData = res.data.principleAssessments[this.principleMap[this.principle]].stepList
+          this.questionId = res.data.principleAssessments[this.principleMap[this.principle]].stepList[0].mainQuestionList[0].id.toString()
+          this.defaultId = this.questionId.toString()
+        }
+      })
+    },
   }
 }
 </script>
 
 <style scoped lang="less">
-
+.el-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 2px solid #E6E6E6;
+}
+.title {
+  height: 64px;
+  display: flex;
+  align-items: center;
+  > div {
+    font-size: 20px;
+    font-weight: bold;
+    color: #000;
+  }
+}
+.backPic {
+  width: 24px;
+  height: 24px;
+  margin-left: 24px;
+  margin-right: 8px;
+}
+#save {
+  padding: 8px 12px;
+  background-color: #78BED3;
+  border-radius: 4px;
+  color: #FFFFFF;
+  margin-right: 24px;
+}
+.notification {
+  display: flex;
+  justify-content: space-between;
+  width: 400px;
+  background-color: #F2F5F7;
+  line-height: 64px;
+}
+.not-box {
+  display: flex;
+  align-items: center;
+  margin-left: 24px;
+  >img {
+    width: 24px;
+    height: 24px;
+  }
+  >span {
+    margin-left: 8px;
+    font-size: 16px;
+    color: #333333;
+  }
+  >div {
+    margin-left: 8px;
+    text-align: center;
+    width: 24px;
+    line-height: 24px;
+    background-color: #FCB215;
+    border-radius: 12px;
+    color: #FFFFFF;
+  }
+}
+.watch {
+  width: 24px;
+  height: 24px;
+  margin-right: 8px;
+}
+.add-subques {
+  cursor: pointer;
+  padding: 5px 12px 5px 8px;
+  background-color: #EDF2F6;
+  border-radius: 4px;
+  display:flex;
+  align-items:center;
+  margin-left: 12px;
+  margin-right: 24px
+}
 </style>
---
+++ vue-ui/src/components/adminTemplate/AdminTemplateMenu.vue
@@ -0,0 +1,319 @@
+<template>
+  <div style="height: 100%;background-color: #F2F2F2">
+    <el-collapse class="BarlowBold" v-model="activeName" accordion>
+      <el-menu :default-active="defaultId" class="myMenu" unique-opened active-text-color="#78BED3" @select="handleSelect">
+        <div class="main-question" v-for="(item1,index1) in menuList" :key="index1">
+          <el-collapse-item :name="item1.step" :style="index1 == 0? 'margin-top: 6px;':''">
+            <!--main question-->
+            <template slot="title" class="BarlowBold">
+              <div class="step-box">
+                <img class="step-pic" style="margin-right: 12px" :src="stepPic[item1.serialNo]" alt="">
+                <span class="collapse-step">{{item1.step}}</span>
+              </div>
+            </template>
+            <el-menu-item class="BarlowMedium" v-for="(item2,index2) in item1.mainQuestionList" :key="item2.id" :index="item2.id.toString()">
+              <!--sub question-->
+              <template slot="title">
+                <div class="ques-list">
+                  <div class="ques-serial">
+                    <div class="serial-font">
+                      {{item2.serial}}
+                    </div>
+                    <div class="ques-img">
+                      <img src="../../assets/adminPic/upPage.png" @click="upRecordMainQues(index2,item1.serialNo)" alt="">
+                      <img src="../../assets/adminPic/downPage.png" @click="downRecordMainQues(index2,item1.serialNo)" alt="">
+                      <img src="../../assets/adminPic/writeProblem.png" @click="editMainQuesFlag[item2.id] = true" alt="">
+                      <img src="../../assets/adminPic/deleteProblem.png" @click="deleteMainQues(item2.id)" alt="">
+                    </div>
+
+                  </div>
+                  <div v-show="!editMainQuesFlag[item2.id]" class="ques-content">{{item2.question}}</div>
+                  <el-input v-show="editMainQuesFlag[item2.id]" v-model="editMainQues[item2.id]" placeholder="Please input a new subquestion">
+                    <i slot="suffix" class="el-input__icon el-icon-check" @click="editMainQuestion(item2.id)"></i>
+                    <i slot="suffix" class="el-input__icon el-icon-close" @click="editMainQuesFlag[item2.id] = false"></i>
+                  </el-input>
+                </div>
+              </template>
+            </el-menu-item>
+            <el-input v-show="addMainQuesFlag[item1.serialNo]" v-model="addMainQues[item1.serialNo]" placeholder="Please input a new main question">
+              <i slot="suffix" class="el-input__icon el-icon-check" @click="addMainQuestion(item1.serialNo)"></i>
+              <i slot="suffix" class="el-input__icon el-icon-close" @click="addMainQuesFlag[item1.serialNo] = false"></i>
+            </el-input>
+            <div class="add-main" @click="addMainQuesFlag[item1.serialNo] = true" v-show="!addMainQuesFlag[item1.serialNo]">
+              <img class="add-main-pic" src="../../assets/questionnairePic/add.svg" alt="">
+            </div>
+          </el-collapse-item>
+        </div>
+      </el-menu>
+    </el-collapse>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AdminTemplateMenu",
+  props: {
+    principle: {
+      type: String,
+      required: true
+    },
+    templateId: {
+      type: String,
+      required: true
+    },
+    menuData: {
+      type: Array,
+      required: true
+    },
+    defaultId: {
+      type: String,
+      required: true
+    }
+  },
+  watch: {
+    'menuData': function () {
+      this.menuList = this.menuData
+      if (this.menuData.length > 0) {
+        for (let i = 0 ; i < this.menuData.length; i++) {
+          if (this.menuData[i].mainQuestionList && this.menuData[i].mainQuestionList.length > 0) {
+            for (let j = 0 ; j < this.menuData[i].mainQuestionList.length; j++) {
+              this.$set(this.editMainQuesFlag,this.menuData[i].mainQuestionList[j].id,false)
+              this.$set(this.editMainQues,this.menuData[i].mainQuestionList[j].id,this.menuData[i].mainQuestionList[j].question)
+            }
+          }
+        }
+      }
+      // console.log('this.menuList',this.menuList)
+    }
+  },
+  data() {
+    return {
+      activeName: 'Principles to Practice',
+      stepPic: {
+        '0': require('../../assets/questionnairePic/portfolio.svg'),
+        '1': require('../../assets/questionnairePic/department.svg'),
+        '2': require('../../assets/questionnairePic/page.svg'),
+        '3': require('../../assets/questionnairePic/issues.svg'),
+        '4': require('../../assets/questionnairePic/screen.svg')
+      },
+      principleMap: {
+        "Generic" : "G",
+        "Fairness" : "F",
+        "Ethics & Accountability" : "EA",
+        "Transparency" : "T"
+      },
+      addMainQuesFlag: {
+        0: false,
+        1: false,
+        2: false,
+        3: false,
+        4: false,
+      },
+      addMainQues: {
+        0: '',
+        1: '',
+        2: '',
+        3: '',
+        4: '',
+      },
+      menuList: this.menuData,
+      editMainQuesFlag: {},
+      editMainQues: {},
+      deleteFlag: false
+    }
+  },
+  created() {
+
+  },
+  methods: {
+    handleSelect(questionId) {
+      if (!this.deleteFlag) {
+        this.$emit("getId",questionId.toString())
+      }
+      this.deleteFlag = false
+    },
+    addMainQuestion(stepNo) {
+      let newMainQues = {}
+      newMainQues.templateId = this.templateId
+      newMainQues.principle = this.principleMap[this.principle]
+      newMainQues.step = stepNo
+      newMainQues.question = this.addMainQues[stepNo]
+      newMainQues.subQuestionList = []
+      this.$http.post(`/api/admin/questionnaire/${this.templateId}/question/new`,newMainQues).then(res => {
+        if (res.status == 200) {
+          this.$emit("getId",res.data.question.id.toString())
+          this.menuList = res.data.toc.principleAssessments[this.principleMap[this.principle]].stepList
+          this.$set(this.editMainQuesFlag,res.data.question.id.toString(),false)
+          this.$set(this.editMainQues,res.data.question.id.toString(),res.data.question.content)
+          this.addMainQues[stepNo] = ''
+          this.addMainQuesFlag[stepNo] = false
+        }
+      })
+    },
+    deleteMainQues(id) {
+      this.deleteFlag = true
+      this.$http.delete(`/api/admin/questionnaire/${this.templateId}/question/${id}`).then(res => {
+        if (res.status == 200) {
+          this.menuList = res.data.principleAssessments[this.principleMap[this.principle]].stepList
+
+        }
+      })
+    },
+    editMainQuestion(id) {
+      let editQues = {}
+      editQues.id = id
+      editQues.mainQuestionId = id
+      editQues.content = this.editMainQues[id]
+      this.$http.post(`/api/admin/questionnaire/${this.templateId}/question/${id}`,editQues).then(res => {
+        if (res.status == 200) {
+          this.$emit("getId",res.data.question.id.toString())
+          this.$emit("getEditFlag",true)
+          this.menuList = res.data.toc.principleAssessments[this.principleMap[this.principle]].stepList
+          this.editMainQuesFlag[id] = false
+        }
+      })
+    },
+    upRecordMainQues(index,step) {
+      let mainQuesRecorder = {}
+      mainQuesRecorder.templateId = this.templateId
+      mainQuesRecorder.principle = this.principleMap[this.principle]
+      mainQuesRecorder.step = step
+      let mainQuesList = this.menuList[step].mainQuestionList.map(item => {
+        return item.id
+      })
+      if(index > 0) {
+        let temp = mainQuesList[index - 1]
+        mainQuesList[index - 1] = mainQuesList[index]
+        mainQuesList[index] = temp
+      } else {
+        mainQuesList.push(mainQuesList.shift())
+      }
+      mainQuesRecorder.questionIdList = mainQuesList
+      this.$http.post(`/api/admin/questionnaire/${this.templateId}/question/reorder`,mainQuesRecorder).then(res => {
+        if (res.status == 200) {
+          if(index > 0) {
+            this.$emit("getId",mainQuesList[index-1].toString())
+          } else {
+            this.$emit("getId",mainQuesList[mainQuesList.length-1].toString())
+          }
+          this.menuList = res.data.principleAssessments[this.principleMap[this.principle]].stepList
+        }
+      })
+    },
+    downRecordMainQues(index,step) {
+      let mainQuesRecorder = {}
+      mainQuesRecorder.templateId = this.templateId
+      mainQuesRecorder.principle = this.principleMap[this.principle]
+      mainQuesRecorder.step = step
+      let mainQuesList = this.menuList[step].mainQuestionList.map(item => {
+        return item.id
+      })
+      if(index < mainQuesList.length-1) {
+        let temp = mainQuesList[index + 1]
+        mainQuesList[index + 1] = mainQuesList[index]
+        mainQuesList[index] = temp
+      } else {
+        mainQuesList.unshift(mainQuesList.pop())
+      }
+      mainQuesRecorder.questionIdList = mainQuesList
+      this.$http.post(`/api/admin/questionnaire/${this.templateId}/question/reorder`,mainQuesRecorder).then(res => {
+        if (res.status == 200) {
+          if(index < mainQuesList.length-1) {
+            this.$emit("getId",mainQuesList[index+1].toString())
+          } else {
+            this.$emit("getId",mainQuesList[0].toString())
+          }
+
+          this.menuList = res.data.principleAssessments[this.principleMap[this.principle]].stepList
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.main-question {
+  border-radius: 8px;
+}
+.el-menu {
+  box-shadow:0px 0px 8px #EDF2F6;
+  background-color: #F2F2F2;
+}
+.el-menu-item {
+  height: auto;
+  white-space: normal;
+  word-break: break-word;
+  line-height: 20px;
+}
+.el-collapse-item {
+  padding: 6px 12px;
+  border-right: none;
+}
+.step-box {
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  font-family: BarlowBold;
+  >img {
+    width: 24px;
+    height: 24px;
+  }
+}
+.ques-list {
+
+}
+.ques-serial {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.serial-font {
+  font-size: 16px;
+  font-family: BarlowBold;
+  width: 48px;
+  text-align: center;
+}
+.ques-img {
+  display: flex;
+  margin-right: 12px;
+  >img {
+    width: 24px;
+    height: 24px;
+    margin-right: 8px;
+  }
+}
+.ques-content {
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  font-family: BarlowMedium;
+  padding: 8px 16px;
+}
+.vertical-line-box {
+  height: calc(100% - 24px);
+  position: relative;
+}
+.vertical-line {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  width: 2px;
+  height: 100%;
+  background-color: #E0E0E0;
+}
+.add-main {
+  display: flex;
+  align-items: center;
+  background: #EDF2F6;
+  border-radius: 4px;
+  margin: 8px 16px;
+  cursor: pointer;
+}
+.add-main-pic {
+  margin: auto;
+  width: 24px;
+  height: 24px;
+}
+</style>
