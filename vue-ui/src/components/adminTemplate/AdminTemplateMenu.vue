b9b32eff7ddf4e021c825b208b2c8bedd0dcdf51
cp <462433864@qq.com>
2023-03-07 10:26:44 +0800

    [add] admin template main ques recorder bug fix

--- vue-ui/src/components/adminTemplate/AdminTemplateMenu.vue
+++ vue-ui/src/components/adminTemplate/AdminTemplateMenu.vue
@@ -20,8 +20,8 @@
                       {{item2.serial}}
                     </div>
                     <div class="ques-img">
-                      <img src="../../assets/adminPic/upPage.png" @click="upRecordMainQues(index2,item1.serialNo)" alt="" v-show="item2.editable">
-                      <img src="../../assets/adminPic/downPage.png" @click="downRecordMainQues(index2,item1.serialNo)" alt="" v-show="item2.editable">
+                      <img src="../../assets/adminPic/upPage.png" @click.stop="upRecordMainQues(index2,item1.serialNo)" alt="" v-show="item2.editable">
+                      <img src="../../assets/adminPic/downPage.png" @click.stop="downRecordMainQues(index2,item1.serialNo)" alt="" v-show="item2.editable">
                       <img src="../../assets/adminPic/writeProblem.png" @click="editMainQuesFlag[item2.id] = true" alt="" v-show="item2.editable">
                       <img src="../../assets/adminPic/deleteProblem.png" @click="deleteMainQues(item2.id)" alt="" v-show="item2.editable">
                     </div>
