7b573d3e6bbb0af7d18ae5da7940a69b15ee415e
Si Xu <xusi1122@gmail.com>
2023-03-10 16:55:16 +0800

    [Fix] converter

--- src/main/java/org/veritas/assessment/biz/converter/QuestionnaireTocDtoConverter.java
+++ src/main/java/org/veritas/assessment/biz/converter/QuestionnaireTocDtoConverter.java
@@ -3,9 +3,10 @@ package org.veritas.assessment.biz.converter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.veritas.assessment.biz.dto.UserSimpleDto;
 import org.veritas.assessment.biz.dto.questionnaire.QuestionnaireTocDto;
+import org.veritas.assessment.biz.entity.Project;
 import org.veritas.assessment.biz.entity.questionnaire.QuestionnaireVersion;
+import org.veritas.assessment.biz.service.ProjectService;
 import org.veritas.assessment.system.entity.User;
 import org.veritas.assessment.system.service.UserService;
 
@@ -16,17 +17,13 @@ public class QuestionnaireTocDtoConverter implements Converter<QuestionnaireTocD
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private ProjectService projectService;
 
     @Override
     public QuestionnaireTocDto convertFrom(QuestionnaireVersion q) {
-        QuestionnaireTocDto dto = new QuestionnaireTocDto();
-        dto.setProjectId(q.getProjectId());
-        dto.setQuestionnaireVid(q.getVid());
-        dto.setCreatedTime(q.getCreatedTime());
-
         User user = userService.findUserById(q.getCreatorUserId());
-        dto.setCreator(new UserSimpleDto(user));
-        dto.fillQuestions(q.getMainQuestionNodeList());
-        return dto;
+        Project project = projectService.findProjectById(q.getProjectId());
+        return new QuestionnaireTocDto(q, project, user);
     }
 }
