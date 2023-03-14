1ca88b4479595c8f516977601236660d3d825a09
Si Xu <xusi1122@gmail.com>
2023-03-14 20:46:57 +0800

    [Mod] add check_lib.py

--- bin/start.sh
+++ bin/start.sh
@@ -6,6 +6,8 @@ if [[ -f py/venv/bin/activate ]]; then
   source py/venv/bin/activate
 fi
 
+python3 py/check_lib.py
+
 JVM_OPT='-Dlog4j2.formatMsgNoLookups=true'
 
 nohup java ${JVM_OPT} -jar $(pwd)/jar/veritas-assessment-tool.jar 1>>log/stdout.log 2>&1 &
