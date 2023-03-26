ed9354550044a97a9f886fac285f149baa8237aa
Si Xu <xusi1122@gmail.com>
2023-03-27 01:39:52 +0800

    [Mod] start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -7,7 +7,8 @@ if [[ -f py/venv/bin/activate ]]; then
 fi
 
 python3 py/check_lib.py
+#JVM_GC_OPT=" -XX:+UseG1GC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps -XX:+UseSerialGC -Xmx1m -Xloggc:./gc-serial.log"
 
-JVM_OPT='-Dlog4j2.formatMsgNoLookups=true'
+JVM_OPT="$JVM_GC_OPT -Dlog4j2.formatMsgNoLookups=true"
 
 nohup java ${JVM_OPT} -jar $(pwd)/jar/veritas-assessment-tool.jar 1>>log/stdout.log 2>&1 &
